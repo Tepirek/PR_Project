51350c3a486baf865c595484a14ac73f6c8d6cef
Canis128 <tomasz.tomaszewski2000@wp.pl>
2021-02-28 15:24:21 +0100

    Area and tower fix

--- client/game/GameObject.js
+++ client/game/GameObject.js
@@ -7,25 +7,9 @@ class GameObject {
         this.size = tileSize;
         this.image = image;
         GameObject.prototype.game = game;
-        this.gameObject = document.createElement('div');
-        this.gameObject.onmouseenter = (e) => this.mouseenter();
-        this.gameObject.onmouseleave = (e) => this.mouseleave();
+        this.gameObject = document.createElement('div');   
         this.draw();
     };
-};
-
-GameObject.prototype.mouseenter = function() {
-    let action = JSON.parse(localStorage.getItem('action'));
-    if(action.type == 'drag') {
-        this.gameObject.style.border = '3px solid #ff1f1f';
-    }
-};
-
-GameObject.prototype.mouseleave = function() {
-    let action = JSON.parse(localStorage.getItem('action'));
-    if(action.type != 'select') {
-        this.gameObject.style.border = '';
-    }
 };
 
 GameObject.prototype.draw = function() {
